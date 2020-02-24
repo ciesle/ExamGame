68d826f7c66436d96bcea14e6d7acac8da8aab26
Thistle <gochitaro6427@gmail.com>
2020-02-24 19:54:20 +0900

    fix bug, add Readme.md

--- ExamGame/Main.cpp
+++ ExamGame/Main.cpp
@@ -1,4 +1,5 @@
-﻿#include <Siv3D.hpp> // OpenSiv3D v0.4.1
+﻿#include <set>
+#include <Siv3D.hpp>
 #include "button.hpp"
 #include "problem.hpp"
 
@@ -11,19 +12,30 @@ void init() {
 	probs.clear();
 	String str;
 	int res = 0;
-	for (int _ = 0; _ < pnames.size(); _++) {
-		auto filename = pnames[_];
+	std::set<String>already;
+	Array<String>names;
+	for (auto g : pnames) names.push_back(g);
+	pnames.clear();
+	for (int _ = 0; _ < names.size(); _++) {
+		auto filename = names[_];
 		if (filename == U"") continue;
+		if (already.find(filename) != already.end()) continue;
+		already.insert(filename);
 		CSVData csv(U"Probs/" + filename + U".csv");
 		if (!csv) {
 			TextReader read(U"Probs/" + filename + U".txt");
 			if (!read) continue;
 			String line;
 			while (read.readLine(line)) {
-				pnames.push_back(line);
+				names.push_back(line);
 			}
 			continue;
 		}
+		else pnames.push_back(filename);
+	}
+	for (int _ = 0; _ < pnames.size(); _++) {
+		auto filename = pnames[_];
+		CSVData csv(U"Probs/" + filename + U".csv");
 		Array<bool>vailds;
 		TextReader reader(U"Probs/Datas/" + filename + U".txt");
 		while (reader.readLine(str)) {
@@ -62,7 +74,6 @@ void drawProb(String& s,int top, Font& font) {
 void Main() {
 	Window::SetTitle(U"CrammingGame");
 	Window::Resize(800, 600);
-	// 背景を水色にする
 	Scene::SetBackground(Palette::Thistle);
 	Font probFont(30);
 	bool pre_mouse = 0;
@@ -87,19 +98,24 @@ void Main() {
 	//3=答えが見られるよ
 	//4=リザルト画面
 	bool probfirst = 1, pfirst2, randomize = 0;
+	String revtext = U"Not Reverse?";
 	while (System::Update())
 	{
 		if (scene == 0) {
 			Title(U"CrammmingGame").drawAt(Scene::CenterF().x, 100, Palette::Gold);
 			Title(U"Ver 2.1.0").drawAt(Scene::CenterF().x, 190, Palette::Gold);
 			start.draw();
+			if (KeyT.pressed() && KeyH.pressed() && KeyI.pressed() && KeyS.pressed()) {
+				Scene::SetBackground(Palette::Gold);
+				revtext = U"Ushitapunichia";
+			}
 			if (KeyEnter.down() || (!pre_mouse && start.onclick())) {
 				scene = 1;
 			}
 		}
 		else if (scene == 1) {
 			Title(U"Settings").drawAt(Scene::CenterF().x, 50);
-			SimpleGUI::CheckBox(probfirst, U"Not Reverse?", Vec2(500, 135));
+			SimpleGUI::CheckBox(probfirst, revtext, Vec2(500, 135));
 			SimpleGUI::CheckBox(randomize, U"Randomize?", Vec2(500, 175));
 
 			setFont(U"File names").drawAt(200, 300);
@@ -110,11 +126,7 @@ void Main() {
 			bool enterdown = KeyEnter.down();
 			if (enterdown || (!pre_mouse && start.onclick())) {
 				if (quest.size() == 0) {
-					TextReader texts(U"Probs/Probs.txt");
-					String str;
-					while (texts.readLine(str)) {
-						pnames << str;
-					}
+					pnames << U"Probs";
 				}
 				else {
 					pnames = quest.split(',');
@@ -150,8 +162,8 @@ void Main() {
 		else if (scene == 2) {
 			drawProb(pfirst2 ? probs[num].prob : probs[num].ans, 100, probFont);
 			pFont(Format(probsize) + U" problems remain").drawAt(Scene::Center().x, 55);
-			pFont(Format((int)Scene::Time() / 60) + U"' " + Format((int)Scene::Time() % 60) + U"''  has left").draw(50, 40);
-			SimpleGUI::CheckBox(probfirst, U"Not Reverse?", Vec2(580, 5));
+			pFont(Format((int)Scene::Time() / 60) + U"' " + Format((int)Scene::Time() % 60) + U"''  have passed").draw(50, 40);
+			SimpleGUI::CheckBox(probfirst, revtext, Vec2(580, 5));
 			SimpleGUI::CheckBox(randomize, U"Randomize?", Vec2(580, 45));
 			OKButton.draw();
 			if (KeyEnter.down() || (!pre_mouse && OKButton.onclick())) {
@@ -159,12 +171,12 @@ void Main() {
 			}
 		}
 		else if (scene == 3) {
-			SimpleGUI::CheckBox(probfirst, U"Not Reverse?", Vec2(580, 5));
+			SimpleGUI::CheckBox(probfirst, revtext, Vec2(580, 5));
 			SimpleGUI::CheckBox(randomize, U"Randomize?", Vec2(580, 45));
 			drawProb(pfirst2 ? probs[num].prob : probs[num].ans, 100, probFont);
 			drawProb(pfirst2 ? probs[num].ans : probs[num].prob, 300, probFont);
 			pFont(Format(probsize) + U" problems remain").drawAt(Scene::Center().x, 55);
-			pFont(Format((int)Scene::Time() / 60) + U"' " + Format((int)Scene::Time() % 60) + U"''  has left").draw(50, 40);
+			pFont(Format((int)Scene::Time() / 60) + U"' " + Format((int)Scene::Time() % 60) + U"''  have passed").draw(50, 40);
 			neverButton.draw();
 			nextButton.draw();
 			if (KeyBackspace.down() || (!pre_mouse && neverButton.onclick())) {
